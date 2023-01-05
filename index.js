0cc6d2fc8cb8510ec13996558c69ec7f0c4a76b1
Aloysius Kang <aloysius.kang1103@gmail.com>
2023-01-05 17:48:32 +0700

    Cleaning JS File

--- index.js
+++ index.js
@@ -2,29 +2,35 @@ const hamburgerEl = document.querySelector("[data-hamburger]")
 const navbar = document.querySelector(".nav__wrapper")
 let animDropdown;
 
+// Function for scaling animation so to not write it twice in event listener
+function scaleAnim(target, value){
+    let ease;
+    if(value == 1){
+        ease = "easeOutQuad";
+    }
+    else{
+        ease = "easeInQuad";
+    }
+
+    anime({
+        targets: target,
+        scale: value,
+        duration: 500,
+        transformOrigin: '90% 0px 0px',
+        easing: ease
+    })
+}
+
+// Still needed for listening clicks and setting the correct value for data- attribute
 hamburgerEl.addEventListener("click", () => {
     let test = hamburgerEl.getAttribute("data-hamburger");
     
     if(test == "true"){
         hamburgerEl.setAttribute("data-hamburger", "false") 
-        animDropdown = anime({
-            targets: navbar,
-            scale: 0,
-            duration: 500,
-            transformOrigin: '90% 0px 0px',
-            easing: 'easeInQuad'
-        })
-        animDropdown.play;
+        scaleAnim(navbar, 0)
     }
     else{
         hamburgerEl.setAttribute("data-hamburger", "true")
-        animDropdown = anime({
-            targets: navbar,
-            scale: 1,
-            duration: 500,
-            transformOrigin: '90% 0px 0px',
-            easing: 'easeOutQuad'
-        })
-        animDropdown.play;
+        scaleAnim(navbar, 1)
     }
 })
